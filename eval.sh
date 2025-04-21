a721d3181ace57f79e3e65db4a45b7501a21798e
吴宇伦 <1746160730@qq.com>
2025-04-21 19:46:59 +0800

    update evaluation code

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=$1 python eval/eval_dtu/dtu_eval.py \
     --input_mesh output/DTU/set_23_24_33/$2/SCAN_NAME/train/ours_15000/fuse_post.ply \
     --output_dir output/DTU/set_23_24_33/$2/SCAN_NAME/train/ours_15000 \
-    --mask /mnt/ssd_1/dc/gaussian-surfels/dtu-pixelnerf \
-    --DTU /mnt/ssd_0/wyl/dtu_gt
+    --mask <mask_dir> \
+    --DTU <gt_dir>
